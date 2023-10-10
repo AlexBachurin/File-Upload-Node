d0c020f06c3f1b2fbbd68bbce23d5df623aa6b44
Aleksandr <alexbachurin91@gmail.com>
2023-10-10 20:55:14 +0300

    upload image on server functionality

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,7 +1,20 @@
+const { STATUS_CODES } = require("http");
 const { StatusCodes } = require("http-status-codes");
+const path = require("path");
 
 const uploadProductImage = async (req, res) => {
-  res.send("upload product image");
+  // store image object we get from req.files
+  const productImage = req.files.image;
+  //construct path
+  const imagePath = path.join(
+    __dirname,
+    "../public/uploads/" + `${productImage.name}`
+  );
+  //move image to our path
+  await productImage.mv(imagePath);
+  return res
+    .status(StatusCodes.OK)
+    .json({ image: { src: `/uploads/${productImage.name}` } });
 };
 
 module.exports = {
