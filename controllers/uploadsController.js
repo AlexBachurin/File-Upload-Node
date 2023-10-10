a8bacb54466a045d7f7ef39dad98b6c7bf93ea5b
Aleksandr <alexbachurin91@gmail.com>
2023-10-10 21:41:42 +0300

    store images in cloudinary now

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,10 +1,28 @@
 const { STATUS_CODES } = require("http");
 const { StatusCodes } = require("http-status-codes");
+const { BadRequestError } = require("../errors");
 const path = require("path");
+const { log } = require("console");
+const cloudinary = require("cloudinary").v2;
 
-const uploadProductImage = async (req, res) => {
+// LOCAL UPLOAD FUNCTIONALITY
+const uploadProductImageLocal = async (req, res) => {
+  // check if file exists
+  if (!req.files) {
+    throw new BadRequestError("No file uploaded");
+  }
+
   // store image object we get from req.files
   const productImage = req.files.image;
+  //   check format(image or not)
+  if (!productImage.mimetype.startsWith("image")) {
+    throw new BadRequestError("Please upload image");
+  }
+  // check size of image
+  const maxSize = 1024 * 1024;
+  if (productImage.size > maxSize) {
+    throw new BadRequestError("Please upload image smaller than 1mb");
+  }
   //construct path
   const imagePath = path.join(
     __dirname,
@@ -15,6 +33,21 @@ const uploadProductImage = async (req, res) => {
   return res
     .status(StatusCodes.OK)
     .json({ image: { src: `/uploads/${productImage.name}` } });
+};
+
+//UPLOAD CLOUDINARY
+const uploadProductImage = async (req, res) => {
+  const result = await cloudinary.uploader.upload(
+    req.files.image.tempFilePath,
+    {
+      use_filename: true,
+      // store in folder
+      folder: "FILE-UPLOAD-NODE",
+    }
+  );
+  //   log(result)
+  //   send back secure url
+  return res.status(StatusCodes.OK).json({ image: { src: result.secure_url } });
 };
 
 module.exports = {
